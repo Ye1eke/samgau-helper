3c61e132fbc9a2d849cd1c48577edc403bcca9b1
yeldos-umbetov <yelekenum@gmail.com>
2022-06-15 10:46:36 +0600

    clear unnecessary lines

--- const.js
+++ const.js
@@ -4,50 +4,5 @@ const commands = `
 /guide - Справочник
 `
 
-const text1 = `
-Some text...
-<a href = "https://www.youtube.com/">Some link</a>
-`
-const text2 = `
-Some text...
-<a href = "https://www.youtube.com/">Some link</a>
-`
-const text3 = `
-Some text...
-<a href = "https://www.youtube.com/">Some link</a>
-`
-const text4 = `
-Some text...
-<a href = "https://www.youtube.com/">Some link</a>
-`
-const text5 = `
-Some text...
-<a href = "https://www.youtube.com/">Some link</a>
-`
-const text6 = `
-Some text...
-<a href = "https://www.youtube.com/">Some link</a>
-`
-const text7 = `
-Some text...
-<a href = "https://www.youtube.com/">Some link</a>
-`
-const text8 = `
-Some text...
-<a href = "https://www.youtube.com/">Some link</a>
-`
-const text9 = `
-Some text...
-<a href = "https://www.youtube.com/">Some link</a>
-`
-
 module.exports.commands = commands
-module.exports.text1 = text1
-module.exports.text2 = text2
-module.exports.text3 = text3
-module.exports.text4 = text4
-module.exports.text5 = text5
-module.exports.text6 = text6
-module.exports.text7 = text7
-module.exports.text8 = text8
-module.exports.text9 = text9
+
